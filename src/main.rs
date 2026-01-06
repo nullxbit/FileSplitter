46de9e63cd208785d1a6347e4d0f276be3a3b3ef
dazai <dazai@localhost.localdomain>
2026-01-06 18:08:12 -0500

    two modes split and merge donegit add .

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,26 @@ use std::{
     path::Path,
 };
 
-fn main() {
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
-        println!("[+] Usage: ./spliter <filename> <chunks>");
+        println!(
+            "[+] Usage: ./spliter --split <file> <chunks>\n ./spliter --merge <file> <chunks> "
+        );
     }
-    split(&args[1], args[2].parse::<u64>().unwrap());
+    //    split(&args[1], args[2].parse::<u64>().unwrap());
+
+    let mode = &args[1];
+    let file = &args[2];
+    let num = args[3].parse::<u64>().unwrap();
+
+    match mode.as_str() {
+        "--split" => split(file, num)?,
+        "--merge" => merge(file, num)?,
+        _ => eprint!("there is a issue"),
+    }
+    Ok(())
 }
 
 // There is no need of nested pattern matching can be done by just using "?" and unwrap here i'm
@@ -56,5 +69,24 @@ fn split<P: AsRef<Path>>(file: P, num: u64) -> io::Result<()> {
 
         //        chunk.push(data);
     }
+    Ok(())
+}
+
+fn merge<P: AsRef<Path>>(file: P, num: u64) -> io::Result<()> {
+    let filename = file.as_ref().to_string_lossy();
+    let merged_name = format!("{}.merged", filename);
+
+    let mut out_file = File::create(&merged_name).unwrap();
+    for i in 0..num {
+        let part_name = format!("{filename}.part{i}");
+        let mut infile = File::open(&part_name).unwrap();
+        let mut buffer = Vec::new();
+        infile.read_to_end(&mut buffer);
+
+        out_file.write_all(&buffer).unwrap();
+        println!("[+] Merged {}", part_name);
+    }
+    println!("[+] Final output: {}", merged_name);
+
     Ok(())
 }
